a7e63b01bc40ddabd168650230877db3cdcecbd3
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-04-24 18:05:47 +0700

    📦 added hexagon, uses indeces, prepare image loading

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,9 @@ struct State {
 
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
     num_vertices: u32,
+    num_indices: u32,
 
     // The window must be declared after the surface so
     // it gets dropped after it as the surface contains
@@ -95,18 +97,28 @@ impl State {
 
         const VERTICES: &[Vertex] = &[
             Vertex {
-                position: [0.0, 0.5, 0.0],
-                color: [1.0, 0.0, 0.0],
-            },
+                position: [-0.0868241, 0.49240386, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // A
             Vertex {
-                position: [-0.5, -0.5, 0.0],
-                color: [0.0, 1.0, 0.0],
-            },
+                position: [-0.49513406, 0.06958647, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // B
             Vertex {
-                position: [0.5, -0.5, 0.0],
-                color: [0.0, 0.0, 1.0],
-            },
+                position: [-0.21918549, -0.44939706, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // C
+            Vertex {
+                position: [0.35966998, -0.3473291, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // D
+            Vertex {
+                position: [0.44147372, 0.2347359, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // E
         ];
+
+        const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
@@ -134,6 +146,7 @@ impl State {
             .filter(|f| f.is_srgb())
             .next()
             .unwrap_or(surface_caps.formats[0]);
+        log::info!("{:#?}", surface_caps);
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: surface_format,
@@ -187,6 +200,18 @@ impl State {
         surface.configure(&device, &config);
 
         let num_vertices = VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        let num_indices = INDICES.len() as u32;
+
+        let diffuse_bytes = include_bytes!("../atlas.png");
+        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+        let diffuse_rgba = diffuse_image.to_rgba8();
+        use image::GenericImageView;
+        let dimensions = diffuse_image.dimensions();
 
         Self {
             window,
@@ -198,6 +223,8 @@ impl State {
             render_pipeline,
             vertex_buffer,
             num_vertices,
+            num_indices,
+            index_buffer,
         }
     }
 
@@ -262,7 +289,8 @@ impl State {
 
             render_pass.set_pipeline(&self.render_pipeline); // 2.
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..3, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
